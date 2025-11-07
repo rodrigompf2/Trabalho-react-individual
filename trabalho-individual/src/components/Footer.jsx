44121b59bacaa64400ef29dcc29a9fb1b0844c2f
rodrigompf2 <rodrigompf15@gmail.com>
2025-11-07 18:52:26 -0300

    Update Footer.jsx

--- trabalho-individual/src/components/Footer.jsx
+++ trabalho-individual/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <p>&copy; {new Date().getFullYear()} Zygos. Todos os direitos reservados.</p>
-    <p>Feito por [Seu Nome Aqui]</p>
+    <p>Feito por [Rodrigo Freitas]</p>
   </footer>
 );
 
